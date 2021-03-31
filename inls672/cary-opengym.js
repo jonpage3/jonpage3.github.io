d2483ef6db95856f8bf8063d635b3a36d88687e6
jonpage3 <jonpage3@gmail.com>
2021-03-31 19:09:23 -0400

    add initial stuff to js

---
+++ inls672/cary-opengym.js
@@ -0,0 +1,35 @@
+
+let findButton = document.querySelector('#findGyms');
+findButton.addEventListener('click', getParks);
+
+var obj;
+
+loadData();
+
+async function loadData() {
+    let url = ' https://data.townofcary.org/api/v2/catalog/datasets/open-gym/exports/json ';
+    try {
+        let response = await fetch(url)};
+        obj = await response.json();
+        console.log(obj);
+
+        let activitySet = new Set();
+        obj.forEach(gym => {
+            activitySet.add(gym.open_gym);
+        });
+
+
+        activityList = Array.from(activitySet).sort();
+        let activityOptions = '';
+
+        activityList.forEach(activity => {
+          let option = `<option>${activity}</option>`;
+          activityOptions += option;
+        });
+
+        let selectOptions = document.querySelector('#activity');
+        selectOptions.innerHTML = featureOptions;
+      } catch (error) {
+        console.log(error);
+}
+
