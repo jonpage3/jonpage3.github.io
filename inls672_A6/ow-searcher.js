0a1b096ea30c4b7781ea004228b211c1b28f0ab5
jonpage3 <jonpage3@gmail.com>
2021-05-02 18:54:47 -0400

    updates to ow-searcher

--- inls672_A6/ow-searcher.js
+++ inls672_A6/ow-searcher.js
@@ -1,22 +1,39 @@
-   let searchButton = document.querySelector('#search');
-   let searchTerm = document.querySelector('#search-term');
-   searchButton.addEventListener('click', loadWeather);
-
-   var obj;        // Global variable for the JSON object created from API response
-
-   // When the user clicks 'Search', fetch the articles for the selected search term
-   async function loadWeather() {
-      let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm.value + '&APPID=4dbc6784d266647f37e36b1e1207323d';
-      try {
-        let response = await fetch(url);
 
 
-        obj = await response.json();
-        console.log(obj);   // Look at this in the Inspector to see what the data structure looks like.
+let searchButton = document.querySelector('#search');
+let searchTerm = document.querySelector('#search-term');
+searchButton.addEventListener('click', loadWeather);
 
-        //getArticles(); // Extract article information from API response and add to HTML web page
+var obj;        // Global variable for the JSON object created from API response
 
-      } catch (error) {
-        console.log(error);
-      }
-    }
+   // When the user clicks 'Search', fetch the data for the weather item
+async function loadWeather() {
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchTerm.value + '&units=imperial&APPID=4dbc6784d266647f37e36b1e1207323d';
+  try {
+     let response = await fetch(url);
+     obj = await response.json();
+     console.log(obj);
+
+     getWeather(); // Extract weather information from API response and add to HTML web page
+
+  } catch (error) {
+     console.log(error);
+  }
+}
+
+function getWeather() {
+
+  //obj.response.docs.forEach(town => {
+    let weatherData = `<section>
+                          <h2>${obj.name}</h2>
+                          <h4>current temperature: ${obj.main.temp} Fahrenheit</h4>
+                          <h5>but feels like... ${obj.main.feels_like}</h5>
+                          <h4>max temperature: ${obj.main.temp_max}  Fahrenheit</h4>
+
+                       </section>`;
+  //});
+
+  let weather_display = document.querySelector('#weather-details');
+  weather_display.innerHTML = weatherData;
+
+}
